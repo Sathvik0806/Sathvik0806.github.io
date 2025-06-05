60347e7c0b5e3ad6061deff3c88634725c33ea46
sathvik <gopishettysathvik@gmail.com>
2025-06-05 19:36:35 +0530

    p2 added

---
+++ temp3/script.js
@@ -0,0 +1,30 @@
+const showLogin = () => {
+    let str=`
+    <div class='App-Container'>
+    <h1>Login Form</h1>
+    <p><input type="text" id="txtEmail"></p>
+    <p><input type="password" id="txtPass"></p>
+    <p><button onclick="showHome()">Log In</button></p>
+    <p><button onclick='showRegister()'>Create Account</button></p>
+    </div>
+    `
+    root.innerHTML = str
+}
+
+const showRegister = () => {
+      let str=`
+    <h1>Register Form</h1>
+    <p><input type="text" id="txtName"></p>
+     <p><input type="text" id="txtEmail"></p>
+    <p><input type="password" id="txtPass"></p>
+    <button>Register</button>
+    <button onclick="showLogin()">If you have an account</button>
+    `
+    root.innerHTML = str
+}
+const showHome = () => {
+    let str=`
+    <h1>Welcome</h1>
+    `
+    root.innerHTML = str
+}
